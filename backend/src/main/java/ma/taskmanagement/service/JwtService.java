52e0d49fff27bd4ed9a8f0dc071e4491ab6a4229
Oumaima <oumaima@Oumaimas-MacBook-Air.local>
2025-04-12 04:07:07 -0700

    add dockerized postgres db

--- backend/src/main/java/ma/taskmanagement/service/JwtService.java
+++ backend/src/main/java/ma/taskmanagement/service/JwtService.java
@@ -6,11 +6,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
 
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
